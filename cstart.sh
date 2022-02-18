46e035a84ac5c37f5f8d949539cbb3f917379e16
snarangaprath <snarangaprath@innophaseinc.com>
2022-02-18 17:12:29 +0530

    cstart: added option for excluding files

--- cstart.sh
+++ cstart.sh
@@ -3,11 +3,12 @@ show_usage () {
     echo
     echo
     echo "Usage:"
-    echo " -a <file/folder> : add files or folders to project list"
-    echo " -c               : Continue even if any of files mentioned in -a is not available"
-    echo " -k               : dont delete db"
-    echo " -fl <listfile>   : use this list of files"
-    echo " -d               : dont consider files in current path"
+    echo " -a  <file/folder> : add files or folders to project list"
+    echo " -ex <file/folder> : exclude files or folders to project list"
+    echo " -c                : Continue even if any of files mentioned in -a is not available"
+    echo " -k                : dont delete db"
+    echo " -fl <listfile>    : use this list of files"
+    echo " -d                : dont consider files in current path"
     echo
     echo
     exit
@@ -16,11 +17,13 @@ show_usage () {
 date
 
 more_files=0
+less_files=0
 retain_db=0
 fl_specified=0
 not_present_continue=0
 dont_consider_current_path_files=0
 rm -f more_files_list
+rm -f less_files_list
 rm -f more_tree
 
 delete_tag_list.sh
@@ -34,7 +37,7 @@ while [ "$#" -gt 0 ]; do
             echo db file fullpath=$fullpath
             if [ -f "$list_file" ]; then
                 echo "$list_file exists."
-            else 
+            else
                 echo "$fullpath is a file.. Doesn't exist.. FAIL..."
                 show_usage
             fi
@@ -51,10 +54,33 @@ while [ "$#" -gt 0 ]; do
             echo "-k"
             shift
             ;;
+        -ex) less_files=1
+            echo "-ex"
+            shift
+            fullpath=`realpath $1`
+            echo exclude_fullpath=$fullpath
+            if [ -d "$fullpath" ]; then
+                echo "$fullpath is a directory."
+                find $fullpath -name '*.[ch]'  >> less_files_list
+            else
+                echo "$fullpath is not a directory."
+                if [ -f "$fullpath" ]; then
+                    echo "$fullpath is a file. exists."
+                    echo $fullpath >> less_files_list
+                else
+                    echo "$fullpath Doesn't exist.."
+                    if [[ $not_present_continue == 1 ]]; then
+                        echo "-c set. skipping and continuing"
+                    else
+                        show_usage
+                    fi
+                fi
+            fi
+            shift
+            ;;
         -a) more_files=1
             echo "-a"
-            shift 
-            newpath=$1
+            shift
             fullpath=`realpath $1`
             echo fullpath=$fullpath
 
@@ -70,7 +96,7 @@ while [ "$#" -gt 0 ]; do
                     echo "$fullpath is a file. exists."
                     echo $fullpath >> more_files_list
                     echo $fullpath >> more_tree
-                else 
+                else
                     echo "$fullpath Doesn't exist.."
                     if [[ $not_present_continue == 1 ]]; then
                         echo "-c set. skipping and continuing"
@@ -108,6 +134,12 @@ if [[ $more_files == 1 ]]; then
     echo "Appending more files..."
     cat more_files_list >> cscope.files
     rm -f more_files_list
+fi
+
+if [[ $less_files == 1 ]]; then
+    realpath $(cat cscope.files) > fullpath
+    grep -vxFf less_files_list fullpath > cscope.files
+    rm -f less_files_list fullpath
 fi
 
 echo "Generating tags using ctags..."
